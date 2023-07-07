1181773257481e1ed858385321b9c4f13bcfe8fc
Your Name <lebomariri505@gmail.com>
2023-07-07 10:10:57 +0200

    updated

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+// use App\Http\Controller\ProductController;
+use App\Http\Controllers\StudentController;
+
+
+Route::get('/', function () {
+     return view('welcome');
+});
+
+Route::resource("/student", StudentController::class) ;
+
